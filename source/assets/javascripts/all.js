2077115674fb57e5d144819c15ef7d3c33a7c260
Alvaro Ruiz <alvaro.ruiz@firstbanco.com>
2015-01-28 17:13:33 +0000

    Adding kudos and background image

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require jquery/dist/jquery
+//= require jquery.cookie/jquery.cookie
 
 // TODO: PICKFRAMEWORK: Remove the one you don't want
 // Watch out for the //= at the beginning. This needs to be present for the
@@ -7,5 +8,8 @@
 //= require bootstrap-sass-official/assets/javascripts/bootstrap.js
 
 //= require application
+//= require kudos
+
+//= require app
 
 
