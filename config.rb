a98b8caa1becddd5195e852db2189f010a18401f
Alvaro Ruiz <alvaro.ruiz@firstbanco.com>
2015-02-04 15:49:53 +0000

    contact. Trying to limit featured post

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".markdown.erb"
   
   blog.new_article_template = "source/layouts/template-article.haml"
 
